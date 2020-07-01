c0edf6b23737a8067007afbc8c2e22aae051f957
Glitch (ellissharp-com) <none>
2020-07-01 14:59:53 +0000

    👐🎢 Updated with Glitch

--- index.js
+++ index.js
@@ -3,23 +3,23 @@ const items = Array.from({ length: pages }, (_, i) => {
   return {
     src: `${i}.jpg`,
     srct: `${i}.jpg`,
-    title: `Chapter ${i}`,
+    title: `Chapter ${i}`
   };
 });
 
-jQuery('#nanogallery2').nanogallery2({
+jQuery("#nanogallery2").nanogallery2({
   itemsBaseURL: `/comics/${comic}/`,
 
-  thumbnailWidth: 'auto',
+  thumbnailWidth: "auto",
   colorScheme: {
     thumbnail: {
-      borderColor: 'rgba(255,255,255,1)',
-    },
+      borderColor: "rgba(255,255,255,1)"
+    }
   },
-  thumbnailAlignment: 'center',
+  thumbnailAlignment: "center",
   displayBreadcrumb: false,
   breadcrumbAutoHideTopLevel: false,
-  thumbnailHoverEffect2: 'scale120|labelAppear75',
+  thumbnailHoverEffect2: "scale120|labelAppear75",
 
-  items,
+  items
 });
